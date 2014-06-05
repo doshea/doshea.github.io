0bf57558f7aeaf8a94640a107c5293e8b0334dc9
Dylan O'Shea <dylan.j.oshea@gmail.com>
2014-06-04 17:31:56 -0700

    Reducing volume

--- js/index.js
+++ js/index.js
@@ -5,6 +5,7 @@ function hide_modal(){
 
 $(document).ready(function(){
   var oblivion = $('#oblivion').get(0);
+  oblivion.volume = 0.4; //it is a loud song
 
   $('button').on('mousedown', function(){
     hide_modal();
